ddfc0a6cf2bff12499c83b4bfa8feace66eebdf5
Ishaan47 <ishaanmathur718@gmail.com>
2020-10-01 14:49:39 +0530

    Angry Bired Stage 4

--- Slingshot.js
+++ Slingshot.js
@@ -5,9 +5,13 @@ class SlingShot{
             pointB: pointB,
             stiffness: 0.04,
             length: 10
+
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.slingImage1 = loadImage("sprites/sling1.png");
+        this.slingImage2 = loadImage("sprites/sling2.png");
+        this.slingImage3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -16,11 +20,27 @@ class SlingShot{
     }
 
     display(){
+        image(this.slingImage1, 200,20);
+        image(this.slingImage2, 170,15);
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            strokeWeight(10);
+            stroke(48, 22, 8);
+           line(pointA.x-20, pointA.y+5, pointB.x-20, pointB.y);
+           line(pointA.x-20, pointA.y+5, pointB.x+20, pointB.y);
+           image(this.slingImage3, pointA.x-20,pointA.y+5);
+           
+           if(pointA.x>220 & pointA.y>20)
+           {
+            line(pointA.x+20, pointA.y+5, pointB.x+20, pointB.y);
+            line(pointA.x+20, pointA.y+5, pointB.x-20, pointB.y);
+            image(this.slingImage3, pointA.x+20,pointA.y+5);
+           }
+          
+           
+
         }
     }
     
